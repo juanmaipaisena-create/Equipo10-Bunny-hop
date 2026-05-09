e42655ff93e4bf564dbb97674d6548a20ea0caf4
juanmaipaisena-create <juanmaipaisena@gmail.com>
2026-05-09 16:53:04 -0300

    Movimiento enemigo serpiente
    Nuevo script para movimiento del enemigo serpiente

--- movimiento_script.gd
+++ movimiento_script.gd
@@ -1,8 +1,10 @@
+class_name ConejoPlayer
 extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
 	var direccion = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	velocity = direccion *230
+	velocity.normalized()
 	move_and_slide()
 
 func _ready() -> void:
