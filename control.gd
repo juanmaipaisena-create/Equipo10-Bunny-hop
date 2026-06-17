4ae11da610a7758bd0a23b1d32ffb0a34e8151b2
Valen_G <valuchi068@gmail.com>
2026-06-17 17:12:32 -0300

    Game over
    Actualización de la escena game over.

--- control.gd
+++ control.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://mundo.tscn")
+	get_tree().change_scene_to_file("res://menu.tscn")
 
 
 func _on_button_2_pressed():
